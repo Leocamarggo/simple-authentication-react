cf7c8ffd8589eeeb22e9008001e02dcc9f40bd8b
leonardo <leonardo.silva@usabit.com.br>
2022-04-29 15:23:05 -0300

    update functions cookie

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -2,7 +2,7 @@ import {api} from '../services/api'
 import { IAuthContext, IUserData } from '../interface/auth'
 import { ChildrenPropsType } from '../interface/children'
 import { createContext, useState, useEffect, useContext } from 'react'
-import { eraseCookie, getCookie, setCookie } from '../utils/FCookies'
+import { eraseCookie, GetCookie, SetCookie } from '../utils/FCookies'
  
 const AuthContext = createContext<IAuthContext>({} as IAuthContext)
 
@@ -10,7 +10,7 @@ export const AuthProvider = ({ children }: ChildrenPropsType) => {
   const [user, setUser] = useState<string | null>(null)
 
   useEffect(() => {
-    const storagedToken = getCookie('@App:token')
+    const storagedToken = GetCookie('@App:token')
 
     storagedToken && setUser(storagedToken)
   }, [])
@@ -20,7 +20,7 @@ export const AuthProvider = ({ children }: ChildrenPropsType) => {
       const response = await api.post('/Login', userData)
 
       setUser(response.data.token)
-      setCookie('@App:token', response.data.token, 7)
+      SetCookie('@App:token', response.data.token, 7)
     } catch (error){
       setUser(null)
     }
--- src/utils/FCookies.ts
+++ src/utils/FCookies.ts
@@ -1,20 +1,20 @@
-
-export function setCookie(name: string,value: string, days:number) {
-  var expires = "";
+export function SetCookie(name: string,value: string, days:number) {
+  let expires = ''
   if (days) {
-      var date = new Date();
+      const date = new Date();
       date.setTime(date.getTime() + (days*24*60*60*1000));
-      expires = "; expires=" + date.toUTCString();
+      expires = "; expires=" + date.toUTCString()
   }
   document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
 
-export function getCookie(name: string) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-      var c = ca[i];
-      while (c.charAt(0) ===' ') c = c.substring(1,c.length);
+export function GetCookie(name: string) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(';');
+
+  for(let i=0;i < ca.length;i++) {
+      let c = ca[i]
+      while (c.charAt(0) ===' ') c = c.substring(1,c.length)
       if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
   }
   return null;
