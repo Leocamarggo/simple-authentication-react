08d8e8c36589e79c7f20841da7cd9331bcbcb934
leonardo <leonardo.silva@usabit.com.br>
2022-04-28 23:14:57 -0300

    update api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({
- baseURL: 'https://reqres.in/api/login',
+export const api = axios.create({
+ baseURL: process.env.REACT_APP_BASE_URL_AUTH,
 });
-
-export default api;
