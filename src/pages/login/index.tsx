5d9b34b80d98b57bc26a8b16279222db37e0e147
leonardo <leonardo.silva@usabit.com.br>
2022-04-30 17:52:11 -0300

    create function get value input

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,18 +1,29 @@
 import * as S from './styles'
 import { useAuth } from '../../contexts/auth'
 import { useNavigate } from 'react-router-dom'
+import { ChangeEvent, useState } from 'react'
+import { IUserData } from '../../interface/auth'
 
 const Login = () => {
   const { login } = useAuth()
   const navigate = useNavigate()
+	const [ loginData, setLoginData ] = useState<IUserData>({email: '', password: ''})
   
   const handleSubmit = async () => {
 		try {
-			await login({ email: "eve.holt@reqres.in", password: "cityslicka" })
+			await login(loginData)
 			navigate('/home')
 		} catch (error) {
 			alert('Error')
 		}
+	}
+
+	const updateData = (
+		id: 'email' | 'password', 
+		event: ChangeEvent<HTMLInputElement>
+	) =>{
+		id === 'email' ? setLoginData({...loginData, email: event.target.value}) 
+		: setLoginData({...loginData, password: event.target.value})
 	}
 
   return (
@@ -24,6 +35,7 @@ const Login = () => {
 					autoComplete='off'
 					required 
 					type='text'
+					onChange={(e)=> updateData('email', e)}
 				/>
 
 				<S.Label htmlFor='password'>SENHA</S.Label>
@@ -32,6 +44,7 @@ const Login = () => {
 					autoComplete='off'
 					required
 					type='password'
+					onChange={(e)=> updateData('password', e)}
 				/>
 				<S.Button onClick={handleSubmit}>ENTRAR</S.Button>
 			</S.Container>
