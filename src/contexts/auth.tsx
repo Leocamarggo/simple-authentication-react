41c9d0d61b5bfbf649569c6807c7840dc974436f
Leonardo <leonardo.camarggo@outlook.com>
2022-07-19 19:58:09 -0300

    apply pattern code

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,9 +1,9 @@
-import {api} from '../services/api'
+import { api } from '../services/api'
 import { IAuthContext, IUserData } from '../interface/auth'
 import { ChildrenPropsType } from '../interface/children'
 import { createContext, useState, useEffect, useContext } from 'react'
 import { deleteCookie, getCookie, setCookie } from '../utils/FCookies'
- 
+
 const AuthContext = createContext<IAuthContext>({} as IAuthContext)
 
 export const AuthProvider = ({ children }: ChildrenPropsType) => {
@@ -14,14 +14,14 @@ export const AuthProvider = ({ children }: ChildrenPropsType) => {
 
     storagedToken && setUser(storagedToken)
   }, [])
-  
+
   async function login(userData: IUserData) {
     try {
-      const response = await api.post('/Login', userData)
-
+      const response = await api.post('/login', userData)
       setUser(response.data.token)
       setCookie('@App:token', response.data.token, 7)
-    } catch (error){
+    } catch (error) {
+      alert('Erro ao fazer Login. Verifique se seu email e senha!')
       setUser(null)
     }
   }
@@ -32,13 +32,15 @@ export const AuthProvider = ({ children }: ChildrenPropsType) => {
   }
 
   return (
-    <AuthContext.Provider value={{ signed: Boolean(user), user,  login, logout }}>
+    <AuthContext.Provider
+      value={{ signed: Boolean(user), user, login, logout }}
+    >
       {children}
     </AuthContext.Provider>
   )
 }
 
-export function useAuth(){
+export function useAuth() {
   const context = useContext(AuthContext)
   return context
- }
+}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,51 +7,51 @@ import { IUserData } from '../../interface/auth'
 const Login = () => {
   const { login } = useAuth()
   const navigate = useNavigate()
-	const [ loginData, setLoginData ] = useState<IUserData>({email: 'eve.holt@reqres.in', password: 'cityslicka'})
-  
-  const handleSubmit = async () => {
-		try {
-			await login(loginData)
-			navigate('/home')
-		} catch (error) {
-			alert('Error')
-		}
-	}
+  const [loginData, setLoginData] = useState<IUserData>({
+    email: 'eve.holt@reqres.in',
+    password: 'cityslicka'
+  })
 
-	const updateData = (
-		id: 'email' | 'password', 
-		event: ChangeEvent<HTMLInputElement>
-	) =>{
-		id === 'email' ? setLoginData({...loginData, email: event.target.value}) 
-		: setLoginData({...loginData, password: event.target.value})
-	}
+  const handleSubmit = async () => {
+    await login(loginData)
+    navigate('/home')
+  }
+
+  const updateData = (
+    id: 'email' | 'password',
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    id === 'email'
+      ? setLoginData({ ...loginData, email: event.target.value })
+      : setLoginData({ ...loginData, password: event.target.value })
+  }
 
   return (
-		<S.Box>
-			<S.Container>
-				<S.Label htmlFor='email'>E-MAIL</S.Label>
-				<S.Input
-					id='email'
-					autoComplete='off'
-					required 
-					type='text'
-					defaultValue={loginData.email}
-					onChange={(e)=> updateData('email', e)}
-				/>
+    <S.Box>
+      <S.Container>
+        <S.Label htmlFor="email">E-MAIL</S.Label>
+        <S.Input
+          id="email"
+          autoComplete="off"
+          required
+          type="text"
+          defaultValue={loginData.email}
+          onChange={(e) => updateData('email', e)}
+        />
 
-				<S.Label htmlFor='password'>SENHA</S.Label>
-				<S.Input
-					id='password'
-					autoComplete='off'
-					required
-					type='password'
-					defaultValue={loginData.password}
-					onChange={(e)=> updateData('password', e)}
-				/>
-				<S.Button onClick={handleSubmit}>ENTRAR</S.Button>
-			</S.Container>
-		</S.Box>
-	)
+        <S.Label htmlFor="password">SENHA</S.Label>
+        <S.Input
+          id="password"
+          autoComplete="off"
+          required
+          type="password"
+          defaultValue={loginData.password}
+          onChange={(e) => updateData('password', e)}
+        />
+        <S.Button onClick={handleSubmit}>ENTRAR</S.Button>
+      </S.Container>
+    </S.Box>
+  )
 }
 
 export default Login
